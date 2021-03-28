5709c0a59e4da4e3ed61ee7bff8f0aa0cd9de7ac
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2021-03-28 15:41:08 +0900

    コピペがバレないように

--- src/xtakumatutix/houti/Main.php
+++ src/xtakumatutix/houti/Main.php
@@ -9,7 +9,7 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
 
-Class Main extends PluginBase implements Listener
+class Main extends PluginBase implements Listener
 {
     public function onEnable()
     {
@@ -18,23 +18,24 @@ Class Main extends PluginBase implements Listener
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
     {
-        if($sender instanceof Player){
+        if ($sender instanceof Player) {
             $name = $sender->getName();
-            if ($this->houti[$name] === false){
+            if ($this->houti[$name] === false) {
                 $this->houti[$name] = true;
-                $this->getServer()->broadcastMessage('§b'.$name.'さんが放置をはじめました');
+                $this->getServer()->broadcastMessage('§b' . $name . 'さんが放置をはじめました');
                 $sender->setImmobile(true);
                 $sender->sendMessage('§bⓘ 放置中は動けません！');
-            }else{
+            } else {
                 $this->houti[$name] = false;
-                $this->getServer()->broadcastMessage('§b'.$name.'さんが放置をやめました');
+                $this->getServer()->broadcastMessage('§b' . $name . 'さんが放置をやめました');
                 $sender->setImmobile(false);
             }
             return true;
         }
     }
 
-    public function onjoin(PlayerJoinEvent $event){
+    public function onjoin(PlayerJoinEvent $event)
+    {
         $event->getPlayer()->setImmobile(false);
         $this->houti[$event->getPlayer()->getName()] = false;
     }
